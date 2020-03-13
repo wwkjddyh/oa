c55540a85a6673e1faeee79716f371aa5b6d3a6c
jianbo <445121408@qq.com>
2020-03-13 20:51:09 +0800

    Update admin.js
    登录界面：验证码验证(不区分大小写)

--- platform/src/main/resources/static/js/custom/admin.js
+++ platform/src/main/resources/static/js/custom/admin.js
@@ -14,14 +14,24 @@ new Vue({
         submitForm() {
             let that = this;
             let error = "";
+            console.log('that.formLogon=>', that.formLogon);
+            let _verifyCode = that.formLogon.verifyCode.replace(/(^\s*)|(\s*$)/g, "");
             let nameLen = that.formLogon.username.replace(/(^\s*)|(\s*$)/g, "").length,
-                pwdLen = that.formLogon.password.length;
+                pwdLen = that.formLogon.password.length,
+                verifyCodeLen = _verifyCode.length;
             if(nameLen <= 2 || nameLen >= 65) {
                 error = "用户名长度范围：3～64 (个字符)";
             }
             else if(pwdLen <= 5 || pwdLen >= 65) {
                 error = "密码长度范围：6~65 (个字符)";
             }
+            else if(verifyCodeLen === 0) {
+                error = "验证码不能为空";
+            }
+            else if(verifyCodeLen != 4) {
+                error = "验证码长度必须为: 4 (个字符)";
+            }
+
             if(error != '') {
                 that.$message({
                     message: error,
@@ -30,7 +40,41 @@ new Vue({
                 });
             }
             else {
-                document.getElementById("logonForm").submit()
+                axios.get("/api/verify/getCode")
+                    .then(function(response) {
+                        let data = response.data;
+                        console.log('data ==>', data)
+                        if(parseInt(data.code) === 200) {
+                            if (data.data.code === _verifyCode.toUpperCase()) {
+                                document.getElementById("logonForm").submit()
+                            }
+                            else {
+                                that.$message({
+                                    message: '验证码错误或失效，可刷新验证码',
+                                    center:true,
+                                    type: 'error'
+                                });
+                                return;
+                            }
+                        }
+                        else {
+                            that.$message({
+                                message: '请刷新验证码',
+                                center:true,
+                                type: 'error'
+                            });
+                            return;
+                        }
+                    })
+                    .catch(function(err) {/*异常*/
+                        console.log(err);
+                        that.$message({
+                            message: '请刷新验证码',
+                            center:true,
+                            type: 'error'
+                        });
+                        return;
+                    });
             }
         },
     },
