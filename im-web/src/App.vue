30c9b7422dca6d7e1fa288145bff0bcac873a6e9
lylalv <lvhongjuan87@126.com>
2020-04-26 02:43:51 +0800

    聊天系统代码上传

---
+++ im-web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <transition>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<style>
+html,body,#app {
+  width: 100%;
+  height: 100%;
+  background-color: #F3F6F8;
+  margin: 0px;
+  padding: 0px;
+}
+.el-button--primary {
+  background-color: #fbbda4;
+  border-color:#fbbda4;
+}
+
+.el-button--primary.is-disabled, .el-button--primary.is-disabled:active, .el-button--primary.is-disabled:focus, .el-button--primary.is-disabled:hover {
+  background-color: #fbdacdcf;
+  border-color: #fbdacdcf;
+}
+</style>
