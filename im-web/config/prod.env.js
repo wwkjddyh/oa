01e308595485d40603d34b3a2c722142c0f51038
lylalv <lvhongjuan87@126.com>
2020-04-26 21:27:21 +0800

    im 生产配置文件地址修改

--- im-web/config/prod.env.js
+++ im-web/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://localhost:8001"',
-  WEBSOCKET_URL: '"ws://localhost:8002"'
+  BASE_API: '"http://localhost:2006"',
+  WEBSOCKET_URL: '"ws://localhost:2007"'
 }
