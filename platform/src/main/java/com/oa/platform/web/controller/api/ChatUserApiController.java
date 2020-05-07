4a5fbb74c66a9217aca9ae448548cc421d6d645e
yuzaosen <1149608772@qq.com>
2020-05-07 21:29:49 +0800

    code

--- platform/src/main/java/com/oa/platform/web/controller/api/ChatUserApiController.java
+++ platform/src/main/java/com/oa/platform/web/controller/api/ChatUserApiController.java
@@ -1,6 +1,5 @@
 package com.oa.platform.web.controller.api;
 
-import org.leo.im.api.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
